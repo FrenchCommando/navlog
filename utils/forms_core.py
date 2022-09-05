4b870921a2795f85530764c4e8964d3743076c51
FrenchCommando <martialren@gmail.com>
2022-09-05 18:51:38 -0400

    some data

--- utils/forms_core.py
+++ utils/forms_core.py
@@ -29,17 +29,18 @@ def fill_contents(d):
             Form.__init__(self, k_navlog)
 
         def build(self):
-            self.d["navlogkey"] = "Blah"
+            pass
 
         def fill_stuff(self):
-            self.d["notes_0"] = "VOR Sparta 111.1"
-            self.d["notes_1"] = "VOR Hughenot 111.1"
+            self.d["notes_0"] = "VOR Sparta SAX115.70"
+            self.d["notes_1"] = "VOR Hughenot HUO116.1"
             self.d["checkpoint_1"] = "KCDW"
             self.d["checkpoint_2"] = "Echo Lake"
             self.d["checkpoint_3"] = "Highland Lake"
             self.d["checkpoint_4"] = "Ridge"
             self.d["checkpoint_5"] = "Pizza42"
             self.d["checkpoint_6"] = "KMSV"
+
             self.d["check_vfr"] = True
             self.d["aircraft_number"] = "734RP"
             self.d["2aircraft_identification"] = "N734RP"
@@ -48,15 +49,16 @@ def fill_contents(d):
             self.d["5departure_point"] = "KCDW"
             self.d["6departure_time_proposed"] = "1200Z"
             self.d["6departure_time_actual"] = "1215Z"
-            self.d["7cruising_altitude"] = "3500"
+            self.d["7cruising_altitude"] = "4500"
             self.d["8route"] = "DCT"
             self.d["9destination"] = "KMSV"
             self.d["10ete_hours"] = "00"
             self.d["10ete_minutes"] = "35"
+            self.d["11remarks"] = "StudentPilot cross country with instructor - Century Air"
             self.d["12fuel_hours"] = "04"
             self.d["12fuel_minutes"] = "00"
             self.d["13alternate"] = "N82"
-            self.d["14pilot"] = "Martial Ren 6469457002 KCDW"
+            self.d["14pilot"] = "FrenchCommando 6461112222 KCDW"
             self.d["15num_aboard"] = "2"
             self.d["16color"] = "W"
 
